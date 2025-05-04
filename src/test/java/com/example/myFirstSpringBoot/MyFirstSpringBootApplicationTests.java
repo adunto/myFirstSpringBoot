c11ecaf84fe4c22a25a33165365343b5c1b8ac17
MinUL <87965992+adunto@users.noreply.github.com>
2025-05-04 13:02:51 +0900

    first commit

---
+++ src/test/java/com/example/myFirstSpringBoot/MyFirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myFirstSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
